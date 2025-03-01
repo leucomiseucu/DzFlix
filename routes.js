b0582b410b1874a881a0d098d3a78a57f8ea16fe
leucomiseucu <dzinloja@gmail.com>
2025-03-01 19:01:03 -0300

    Create routes.js

---
+++ routes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path');
+
+const router = express.Router();
+
+// Rota /inicio -> Servir index.html
+router.get('/inicio', (req, res) => {
+    res.sendFile(path.join(__dirname, '../index.html')); // Caminho correto para index.html
+});
+
+module.exports = router;
